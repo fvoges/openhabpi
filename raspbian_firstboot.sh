d35184a1c109f79fb2a1cb153a284e53848abfa2
hlw31 de <hlw31.de@gmail.com>
2013-07-10 18:50:44 +0200

    Chose raspbian base image for sleak image adjusted install instructions

--- raspbian_firstboot.sh
+++ raspbian_firstboot.sh
@@ -7,6 +7,13 @@ rm /etc/ssh/ssh_host_* && dpkg-reconfigure openssh-server
 apt-get update
 
 # Install & configure NTP
-apt-get -y install ntp fake-hwclock
+apt-get -y install ntp fake-hwclock vim
 echo "Europe/Berlin" > /etc/timezone
 dpkg-reconfigure -f noninteractive tzdata
+
+#
+# Change hostname to openhab
+#
+hostname openhab
+echo openhab > /etc/hostname
+echo "127.0.0.1 openhab" >> /etc/hosts
